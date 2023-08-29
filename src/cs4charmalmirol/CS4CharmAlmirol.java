bd7741f133dd252974edafca7d1d43e0ff68c78e
CHARM <CHARM@172.22.16.71>
2023-08-29 16:14:11 +0800

    first java project

---
+++ src/cs4charmalmirol/CS4CharmAlmirol.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4charmalmirol;
+
+/**
+ *
+ * @author CHARM
+ */
+public class CS4CharmAlmirol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        var member = "Eclair";
+        var memberItem = "Ice Cream Sandwich";
+        System.out.println(member + ", look at this " + memberItem + "!");
+    }
+    
+}
